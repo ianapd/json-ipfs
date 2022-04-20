932835c66a17580f89620dcde86460bbc2dff491
Ian Sui <ian.appdeture@gmail.com>
2022-04-20 15:00:49 +0800

    first commit

--- pages/form.js
+++ pages/form.js
@@ -1,6 +1,6 @@
-import { Box, Container, Heading, FormControl, FormLabel, Input, Button, Textarea, Link, Text, VStack, Image, Spinner } from "@chakra-ui/react";
+import { Box, Container, Heading, FormControl, FormLabel, Input, Button, Textarea, Link, Text, VStack, Image } from "@chakra-ui/react";
 import { create } from "ipfs-http-client"
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
 const itemState = {
   item: '',
@@ -12,21 +12,14 @@ const itemState = {
 
 const client = create("https://ipfs.infura.io:5001/api/v0")
 
-export default function Home() {  
+export default function Form () {
   const [item, setItem] = useState(itemState)
   const [cid, setCid] = useState("")
   const [fileUrl, setFileUrl] = useState("")
-  const [jsonData, setJsonData] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
+  const [jsonData, setJsonData] = useState([])
 
   async function onChange ({ target: { name, value, files } }) {
     switch (name) {
-      case 'royalties':
-        setItem({ ...item, [name]: parseInt(value) })
-        break;
-      case 'price':
-        setItem({ ...item, [name]: parseFloat(value) })
-        break;  
       case 'edition':
         setItem({ ...item, [name]: `1/${value}` })
         break;
@@ -51,7 +44,6 @@ export default function Home() {
   }
 
   async function upload() {
-    setIsLoading(true)
     const jsonData = JSON.stringify(item)
     try {
       const added = await client.add(jsonData)
@@ -71,7 +63,6 @@ export default function Home() {
       const data = await fetch(fileUrl)
       const json = await data.json()
       setJsonData(json)
-      setIsLoading(false)
     } catch (error) {
       console.log('Error getting json data: ', error)
     }  
@@ -79,14 +70,12 @@ export default function Home() {
 
   useEffect(() => {
     getJsonData(fileUrl)
-    console.log(jsonData)
   }, [fileUrl])
 
   return (
     <Box pt={16}>
       <Container maxW="container.md">
-        <Heading mb={4}>Upload JSON Data to IPFS and get the data</Heading>
-        <Heading size="lg" mb={4}>Create an Item</Heading>
+        <Heading mb={4}>Create an Item</Heading>
         <FormControl mb={4}>
           <FormLabel>Upload MP3, MP4, JPG AND PNG</FormLabel>
           <Input name="file" type="file" onChange={onChange} />
@@ -113,30 +102,31 @@ export default function Home() {
         </FormControl>
         <Button onClick={upload} mb={4}>Upload to IPFS</Button>
         <VStack align="start" spacing={0}>
+          {
+            fileUrl && (
+              <Link mb={4} as="a" href={fileUrl} target="_blank">
+                Go to the link of json metadata
+              </Link>
+            )
+          }
+          {
+            cid && (
+              <Text mb={4}>Cid: {cid}</Text>
+            )
+          }
         </VStack>
+        <Heading size="md" mb={4}>Data from JSON</Heading>
         {
-          isLoading && <Spinner />
-        }
-        {
-          jsonData ? (
-            <VStack align="start" spacing={2} mb={8}>
-              <Heading size="md" my={4}>Data from JSON</Heading>
-              <Image src={jsonData.file} boxSize="200px" />
-              <Text>Cid: {cid}</Text>
+          jsonData && (
+            <VStack align="start" spacing={0}>
+              <Image mb={4} src={jsonData.file} />
               <Text>Item Name: {jsonData.item}</Text>
               <Text>Description: {jsonData.description}</Text>
               <Text>Royalties: {jsonData.royalties}</Text>
               <Text>Edition: {jsonData.edition}</Text>
               <Text>Price: {jsonData.price}</Text>
-              {
-                fileUrl && (
-                  <Link mb={2} as="a" href={fileUrl} target="_blank">
-                    <Button>Get the link</Button>
-                  </Link>
-                )
-              }
             </VStack>
-          ) : null
+          )
         }  
       </Container>  
     </Box>
